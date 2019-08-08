7cc7e1a97c3b99ebf1aecbd10e7598b49a87b833
Shujauddin Mazhar Mohammad <shuja.mazhar62@gmail.com>
2019-08-08 10:29:03 -0700

    updated msg again

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello World! Hello";
+    private final String message = "Hello World! Hopefully now";
 
     public App() {}
 
